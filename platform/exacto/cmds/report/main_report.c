097aebe1eab9ed62385c2d1a4b6908c5ff771ebc
Artem <artem.o.kuznecov@gmail.com>
2020-12-29 08:59:53 +0300

    add reporter starter kit

---
+++ platform/exacto/cmds/report/main_report.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>  
+#include <stdint.h>
+
+
+
+int main(int argc, char *argv[]) {
+    printf("Hello, I'm Mline main\n");
+    return 0;
+}
