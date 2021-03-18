c4375e31b92d56594bf672fa497c616861bc7ae1
Artem <artem.o.kuznecov@gmail.com>
2021-03-18 12:55:58 +0300

    fx

--- platform/exacto/afina/spi/spi1.c
+++ platform/exacto/afina/spi/spi1.c
@@ -98,7 +98,6 @@ static int SPI1_FULL_DMA_init(void)
 
     GPIO_InitStruct.Pin = LL_GPIO_PIN_7;
     GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.OutputType =    LL_GPIO_PULL_DOWN;
     // GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
