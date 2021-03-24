838ce1d612cce8980b707c4d5f1252d5961c6d29
Artem <artem.o.kuznecov@gmail.com>
2021-03-24 09:08:42 +0300

    add blinker code

---
+++ platform/exacto/afina/blink/blinker.c
@@ -0,0 +1,26 @@
+#include "stm32f7xx_ll_gpio.h"
+#include <errno.h>
+#include <embox/unit.h>
+
+
+EMBOX_UNIT_INIT(initAfinaBlinkBasis);
+static int initAfinaBlinkBasis(void)
+{
+    return 0;
+}
+void setSysLedOn()
+{
+
+}
+void setSysLedOff()
+{
+
+}
+void setDataLedOn()
+{
+
+}
+void setDataLedOff()
+{
+
+}
