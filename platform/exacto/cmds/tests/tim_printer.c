bf7fb61e521a065de543f5ec6a0182f269f662be
Artem <artem.o.kuznecov@gmail.com>
2021-05-01 22:13:00 +0300

    fixing

--- platform/exacto/cmds/tests/tim_printer.c
+++ platform/exacto/cmds/tests/tim_printer.c
@@ -5,6 +5,7 @@
 #include <string.h>
   
 #include <util/err.h>
+
 #include <kernel/sched.h>
 #include <kernel/sched/waitq.h>
 #include <kernel/sched/schedee_priority.h>
@@ -12,32 +13,35 @@
 #include <kernel/thread.h>
 #include <kernel/thread/sync/mutex.h>
 #include <kernel/thread/sync/cond.h>
-#include <kernel/time/ktime.h>
-#include <kernel/lthread/sync/mutex.h>
 #include <kernel/lthread/lthread.h>
+#include <kernel/lthread/sync/mutex.h>
 #include <kernel/task.h>
+#include <kernel/time/ktime.h>
+
+
+
 #include <sys/wait.h>
 
 #include "tim/tim.h"
 #include "commander/exacto_filemanager.h"
-
-
-uint8_t Counter = 0;
-
-uint8_t MarkerSubscribe = 0;
-
-struct lthread SubscribeThread;
-struct lthread MainLightThread;
-
-static struct thread *BasicThread;
-static cond_t SignalFromTim;
-static struct mutex MutexTim;
-
 #define MAX_DIVIDER 10
-uint8_t DividerIndex = 0;
+
+uint8_t TmPrDividerIndex = 0;
+
+uint8_t TmPrCounter = 0;
+
+uint8_t TmPrMarkerSubscribe = 0;
+
+static struct lthread TmPrSubscribeThread;
+static struct lthread TmPrMainLightThread;
+
+static struct thread *TmPrBasicThread;
+static cond_t TmPrSignalFromTim;
+static struct mutex TmPrMutexTim;
 
 
-static void *runBasicThread(void *arg) {
+
+static void *runTmPrBasicThread(void *arg) {
     printf("Basic full thread run\n");
     uint8_t buffer0[] = "type typ type from common thread\n";
 
@@ -45,69 +49,75 @@ static void *runBasicThread(void *arg) {
     {
         printf("Try to save to file\n");
         ex_saveToFile(buffer0, sizeof(buffer0));
-        mutex_lock(&MutexTim);
-        cond_wait(&SignalFromTim, &MutexTim);
+        mutex_lock(&TmPrMutexTim);
+        cond_wait(&TmPrSignalFromTim, &TmPrMutexTim);
     }
 	return NULL;
 }
 
-
-static int runMainLightThread(struct lthread * self)
+static int runTmPrMainLightThread(struct lthread * self)
 {
-	if (mutex_trylock_lthread(self, &MutexTim) == -EAGAIN) {
-		return 0;
+    start:
+    mutex_retry:
+	if (mutex_trylock_lthread(self, &TmPrMutexTim) == -EAGAIN) {
+        return lthread_yield(&&start, &&mutex_retry);
 	}
-    printf("Light thread run\n");
-    cond_signal(&SignalFromTim);
+    // printf("Light thread run\n");
+    cond_signal(&TmPrSignalFromTim);
 	// test_emit('d');
-	mutex_unlock_lthread(self, &MutexTim);
+	mutex_unlock_lthread(self, &TmPrMutexTim);
     // uint8_t buffer0[] = "type typ type from light thread\n";
 
     // ex_saveToFile(buffer0, sizeof(buffer0));
     return 0;
 }
-static int runPrinter(struct  lthread * self)
+static int runTmPrPrinter(struct  lthread * self)
 {
-    printf("Print: %d\n", Counter);
-    Counter++;
-    if (DividerIndex < MAX_DIVIDER)
+    printf("Print: %d\n", TmPrCounter);
+    TmPrCounter++;
+    if (TmPrDividerIndex < MAX_DIVIDER)
     {
-        DividerIndex++;
+        TmPrDividerIndex++;
     }
     else
     {
-        DividerIndex = 0;
-        lthread_launch(&MainLightThread);
+        TmPrDividerIndex = 0;
+        lthread_launch(&TmPrMainLightThread);
     }
     return 0;
 }
-static int runSubcribeThread( struct lthread * self)
+static int runTmPrSubcribeThread( struct lthread * self)
 {
-    uint8_t result = ex_subscribeOnEvent(&ExTimServicesInfo, ExTimServices, THR_TIM, runPrinter);
+    uint8_t result = ex_subscribeOnEvent(&ExTimServicesInfo, ExTimServices, THR_TIM, runTmPrPrinter);
     if (result == 0)
-        MarkerSubscribe = 1;
+        TmPrMarkerSubscribe = 1;
     return 0;
 }
 
 int main(int argc, char *argv[]) {
+	int l = 200, h = 210;
     printf("Start print function\n");
     initExactoFileManager();
 
-    lthread_init(&SubscribeThread, runSubcribeThread);
-    lthread_init(&MainLightThread, runMainLightThread);
+    lthread_init(&TmPrSubscribeThread, runTmPrSubcribeThread);
+    lthread_init(&TmPrMainLightThread, runTmPrMainLightThread);
 
-	BasicThread = thread_create(THREAD_FLAG_SUSPENDED, runBasicThread, NULL);
-	mutex_init(&MutexTim);
-	cond_init(&SignalFromTim, NULL);
+	TmPrBasicThread = thread_create(THREAD_FLAG_SUSPENDED, runTmPrBasicThread, NULL);
+	mutex_init(&TmPrMutexTim);
+	cond_init(&TmPrSignalFromTim, NULL);
+    
+    schedee_priority_set(&TmPrMainLightThread.schedee,l);
+    schedee_priority_set(&TmPrBasicThread->schedee,h);
 
-    thread_launch(BasicThread);
-    thread_join(BasicThread, NULL);
 
-    lthread_launch(&SubscribeThread);
+    thread_launch(TmPrBasicThread);
+    thread_join(TmPrBasicThread, NULL);
+
+    lthread_launch(&TmPrSubscribeThread);
 
     printf("Wait subscribing...\n");
 
-    while (!MarkerSubscribe)
+    while (!TmPrMarkerSubscribe)
     {
     }
     
