38ca3cb612987c1d6a82717d0e7ccabce7dd2f32
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2021-10-26 02:12:55 +0300

    basics i2c updates

--- platform/exacto/i2c/i2c_afina_mliner.c
+++ platform/exacto/i2c/i2c_afina_mliner.c
@@ -27,32 +27,121 @@
 #include "commander/exacto_data_storage.h"
 
 
+
 typedef struct
 {
     struct lthread dt_lth;
+    uint8_t dt_buffer[BUFFER_SIZE_I2C_MLINER];
     uint8_t is_enabled;
+    uint16_t dt_count;
 } buffer;
 
-static buffer TX_buffer = {
+static buffer RX_buffer = {
 	.is_enabled = 0,
+	.dt_count = BUFFER_SIZE_I2C_MLINER,
 };
 
 EMBOX_UNIT_INIT(init_I2C_MLINER);
 static int init_I2C_MLINER(void)
 {
+	/* general cubemx section */
+  LL_I2C_InitTypeDef I2C_InitStruct = {0};
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+  /**I2C1 GPIO Configuration
+  PB6   ------> I2C1_SCL
+  PB7   ------> I2C1_SDA
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_7;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* Peripheral clock enable */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C1);
+
+    /* I2C1_RX Init */
+  LL_DMA_SetChannelSelection(DMA1, LL_DMA_STREAM_0, LL_DMA_CHANNEL_1);
+  LL_DMA_SetDataTransferDirection(DMA1, LL_DMA_STREAM_0, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
+  LL_DMA_SetStreamPriorityLevel(DMA1, LL_DMA_STREAM_0, LL_DMA_PRIORITY_LOW);
+  LL_DMA_SetMode(DMA1, LL_DMA_STREAM_0, LL_DMA_MODE_NORMAL);
+  LL_DMA_SetPeriphIncMode(DMA1, LL_DMA_STREAM_0, LL_DMA_PERIPH_NOINCREMENT);
+  LL_DMA_SetMemoryIncMode(DMA1, LL_DMA_STREAM_0, LL_DMA_MEMORY_INCREMENT);
+  LL_DMA_SetPeriphSize(DMA1, LL_DMA_STREAM_0, LL_DMA_PDATAALIGN_BYTE);
+  LL_DMA_SetMemorySize(DMA1, LL_DMA_STREAM_0, LL_DMA_MDATAALIGN_BYTE);
+  LL_DMA_DisableFifoMode(DMA1, LL_DMA_STREAM_0);
+
+  /* I2C1_TX Init */
+  LL_DMA_SetChannelSelection(DMA1, LL_DMA_STREAM_6, LL_DMA_CHANNEL_1);
+  LL_DMA_SetDataTransferDirection(DMA1, LL_DMA_STREAM_6, LL_DMA_DIRECTION_MEMORY_TO_PERIPH);
+  LL_DMA_SetStreamPriorityLevel(DMA1, LL_DMA_STREAM_6, LL_DMA_PRIORITY_LOW);
+  LL_DMA_SetMode(DMA1, LL_DMA_STREAM_6, LL_DMA_MODE_NORMAL);
+  LL_DMA_SetPeriphIncMode(DMA1, LL_DMA_STREAM_6, LL_DMA_PERIPH_NOINCREMENT);
+  LL_DMA_SetMemoryIncMode(DMA1, LL_DMA_STREAM_6, LL_DMA_MEMORY_INCREMENT);
+  LL_DMA_SetPeriphSize(DMA1, LL_DMA_STREAM_6, LL_DMA_PDATAALIGN_BYTE);
+  LL_DMA_SetMemorySize(DMA1, LL_DMA_STREAM_6, LL_DMA_MDATAALIGN_BYTE);
+  LL_DMA_DisableFifoMode(DMA1, LL_DMA_STREAM_6);
+
+	LL_I2C_EnableAutoEndMode(I2C1);
+  LL_I2C_SetOwnAddress2(I2C1, 0, LL_I2C_OWNADDRESS2_NOMASK);
+  LL_I2C_DisableOwnAddress2(I2C1);
+  LL_I2C_DisableGeneralCall(I2C1);
+  LL_I2C_EnableClockStretching(I2C1);
+  I2C_InitStruct.PeripheralMode = LL_I2C_MODE_I2C;
+  I2C_InitStruct.Timing = 0x20404768;
+  I2C_InitStruct.AnalogFilter = LL_I2C_ANALOGFILTER_ENABLE;
+  I2C_InitStruct.DigitalFilter = 0;
+  I2C_InitStruct.OwnAddress1 = 0;
+  I2C_InitStruct.TypeAcknowledge = LL_I2C_ACK;
+  I2C_InitStruct.OwnAddrSize = LL_I2C_OWNADDRESS1_7BIT;
+  LL_I2C_Init(I2C1, &I2C_InitStruct);
+
+	/* DMA config */
+    // DMA1_STREAM_0 -> RX
+    LL_DMA_ConfigAddresses(DMA1, 
+                            LL_DMA_STREAM_0,
+                           LL_I2C_DMA_GetRegAddr(I2C1), (uint32_t)RX_buffer.dt_buffer,
+                           LL_DMA_GetDataTransferDirection(DMA1, LL_DMA_STREAM_0));
+    LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_0, RX_buffer.dt_count);    
+    // DMA1_STREAM_6 -> TX
+
+
+    /* DMA interrupts */
+    LL_DMA_EnableIT_TC(DMA1, LL_DMA_STREAM_0);
+    LL_DMA_EnableIT_TE(DMA1, LL_DMA_STREAM_0); 
+
+    /* embox specific section  */
+    irq_attach(11, RX_DmaHandler, 0, NULL, "I2C1 rx DMA handler");
+
+    LL_I2C_EnableDMAReq_RX(I2C1);
+    /* enable i2c */
+    LL_I2C_Enable(I2C1);
+
 	return 0;
 }
 
-static irq_return_t TX_DmaHandler(unsigned int irq_nr, void *data)
+static irq_return_t RX_DmaHandler(unsigned int irq_nr, void *data)
 {
     if (LL_DMA_IsActiveFlag_TC0(DMA1) != RESET)
     {
         LL_DMA_ClearFlag_TC0(DMA1);
-	lthread_launch(&TX_buffer.dt_lth);
+	lthread_launch(&RX_buffer.dt_lth);
     }
     return IRQ_HANDLED;
 }
-STATIC_IRQ_ATTACH(11, TX_DmaHandler, NULL); 
+STATIC_IRQ_ATTACH(11, RX_DmaHandler, NULL); 
 //  DMA1_Stream0_IRQn           = 11,     /*!< DMA1 Stream 0 global Interrupt
 
 //DMA1_Stream6_IRQn           = 17,     /*!< DMA1 Stream 6 global Interrupt   
@@ -68,4 +157,11 @@ void bind_RX_thread_I2C_MLINER(int (*run)(struct lthread *))
 {
 
 }
+void transmit_I2C_MLINER(uint8_t * data, const uint16_t datalen)
+{
 
+}
+void receive_I2C_MLINER(uint8_t * data, const uint16_t datalen)
+{
+	
+}
--- platform/exacto/i2c/i2c_mliner.h
+++ platform/exacto/i2c/i2c_mliner.h
@@ -6,4 +6,6 @@
 
 extern void bind_TX_thread_I2C_MLINER(int (*run)(struct lthread *));
 extern void bind_RX_thread_I2C_MLINER(int (*run)(struct lthread *));
+extern void transmit_I2C_MLINER(uint8_t * data, const uint16_t datalen);
+extern void receive_I2C_MLINER(uint8_t * data, const uint16_t datalen);
 #endif //
