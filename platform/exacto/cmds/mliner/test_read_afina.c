94774181490dcc22bbb5b271c51339fd7e1d6cd2
artem <artem.o.kuznecov@gmail.com>
2022-01-20 20:23:36 +0300

    cmd fix done

--- platform/exacto/cmds/mliner/test_read_afina.c
+++ platform/exacto/cmds/mliner/test_read_afina.c
@@ -23,20 +23,48 @@
 
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+    int index_max = -1, var_cnt = 3;
+	int opt;
+    extern char * optarg;
+	while((opt = getopt(argc, argv, "M:f:h")) != -1)
+    {
+		switch (opt) {
+		case 'M':
+            if (optarg) 
+                index_max = atoi(optarg);
+			break;
+		case 'f':
+            if (optarg) 
+                var_cnt = atoi(optarg);
+            break;
+		case 'h':
+            printf("Usage:\ntest_read -- infinity number of readings\ntest_read -M [number of reading iterations]\n");
+           return 0; 
+        }
+	} 
     printf("Test read state\n");
     int index = 0;
     while(1)
     {
         uint32_t * val = 0;
         uint8_t res = 0;
-        getMlinerVars(DATACHECK_COUNTER, val, &res );
-        printf("[%d]Datacheck: %d \n", index, *val);
-        getMlinerVars(TX_BUFFER, val, &res );
-        printf("TX: %d \n", *val);
-        getMlinerVars(RX_BUFFER, val, &res );
-        printf("RX: %d \n", *val);
+        if (var_cnt == 3)
+        {
+            getMlinerVars(DATACHECK_COUNTER, val, &res );
+            printf("[%d]Datacheck: %d \n", index, *val);
+            getMlinerVars(TX_BUFFER, val, &res );
+            printf("TX: %d \n", *val);
+            getMlinerVars(RX_BUFFER, val, &res );
+            printf("RX: %d \n", *val);
+        }
         index++;
+        if (index_max != -1)
+        {
+            if (index > index_max)
+                break;
+        }
         usleep(1000000);
     }
     printf("Done\n");
