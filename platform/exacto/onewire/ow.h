39858e10917b2b3c7fc9a8a85d8c616fd4eab35e
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2022-08-11 22:35:56 +0300

    init onewire

---
+++ platform/exacto/onewire/ow.h
@@ -0,0 +1,18 @@
+#ifndef OW_H_
+#define OW_H_
+
+#include <stdint.h>
+
+#define EX_OW_PACK_SZ 32
+
+typedef struct{
+    uint8_t data[EX_OW_PACK_SZ];
+    uint8_t datalen;
+    uint8_t cmd;
+}ex_ow_pack_t;
+
+extern uint8_t ex_onewire_reset();
+extern uint8_t ex_onewire_send();
+extern uint8_t ex_onewire_read();
+
+#endif
