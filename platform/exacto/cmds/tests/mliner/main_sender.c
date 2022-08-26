9df8e4eaebc5de74fb8be33cdeff331e805eb502
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2022-08-26 23:52:41 +0300

    sm fx

---
+++ platform/exacto/cmds/tests/mliner/main_sender.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) 
+{
+    int index_max = -1, var_cnt = 3;
+	int opt;
+    extern char * optarg;
+	while((opt = getopt(argc, argv, "M:f:h")) != -1)
+    {
+		switch (opt) {
+		case 'M':
+            if (optarg) 
+                index_max = atoi(optarg);
+			break;
+		case 'f':
+            if (optarg) 
+                var_cnt = atoi(optarg);
+            break;
+		case 'h':
+            printf("Usage:\ntest_read -- infinity number of readings\ntest_read -M [number of reading iterations]\n");
+           return 0; 
+        }
+	} 
+return 1;
+}
