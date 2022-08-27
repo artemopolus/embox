6cc739169462bbf2f7190d909d9f3b7ebf70f56e
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2022-08-27 09:51:25 +0300

    sm fx

--- platform/exacto/cmds/tests/mliner/main_sender.c
+++ platform/exacto/cmds/tests/mliner/main_sender.c
@@ -4,9 +4,17 @@
 #include <errno.h>
 #include <string.h>
 
+static void sending()
+{
+
+}
+static void init()
+{
+
+}
 int main(int argc, char *argv[]) 
 {
-    int index_max = -1, var_cnt = 3;
+    int index_max = -1, var_cnt = 2;
 	int opt;
     extern char * optarg;
 	while((opt = getopt(argc, argv, "M:f:h")) != -1)
@@ -21,9 +29,12 @@ int main(int argc, char *argv[])
                 var_cnt = atoi(optarg);
             break;
 		case 'h':
-            printf("Usage:\ntest_read -- infinity number of readings\ntest_read -M [number of reading iterations]\n");
+            printf(
+                "Usage:\ntest_read -- infinity number of readings\ntest_read -M [number of reading iterations, default = inf]\n-f [number of device to search, default = 2]");
            return 0; 
         }
-	} 
-return 1;
+	}
+    init();
+    sending();
+    return 1;
 }
