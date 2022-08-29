824356d0c0b25e14b83fbae5d11b2c9c9a416615
Artem <artem.o.kuznecov@gmail.com>
2022-08-29 16:52:50 +0300

    add timer to apollon

--- platform/exacto/cmds/tests/mliner/sec_getter.c
+++ platform/exacto/cmds/tests/mliner/sec_getter.c
@@ -14,32 +14,55 @@
 #include "exlnk_getHeader.h"
 #include "exlnk_Cmd.h"
 
+#include "tim/tim.h"
+
 #define ECTM_MESSAGE_SIZE EXACTO_BUFFER_UINT8_SZ
-uint8_t ECTM_TransmitBuffer[ECTM_MESSAGE_SIZE] = {0};
-uint8_t ECTM_ReceiveBuffer[ECTM_MESSAGE_SIZE] = {0};
+static uint8_t ECTM_TransmitBuffer[ECTM_MESSAGE_SIZE] = {0};
+static uint8_t ECTM_ReceiveBuffer[ECTM_MESSAGE_SIZE] = {0};
 static uint32_t ECTM_SendData_Counter = 0;
 
-exlnk_set_header_str_t SendBuffer;
-exlnk_get_header_str_t GettBuffer;
+static exlnk_set_header_str_t SendBuffer;
+static exlnk_get_header_str_t GettBuffer;
 
-uint8_t TmpBuffer[100];
+static uint8_t TmpBuffer[100];
 
-static void sending(int value)
+static uint8_t NeedToSend = 0;
+
+// static struct lthread Tim_Lthread;
+static int run_Tim_Lthread(struct  lthread * self)
 {
-	receiveExactoDataStorage();
+	checking();
+	return 0;
+}
 
-	printf("iteration[%d]\n", ECTM_SendData_Counter);
-	exds_getData(ECTM_ReceiveBuffer, ECTM_MESSAGE_SIZE, 0); 
-
-
-	exlnk_getHeader(ECTM_ReceiveBuffer, ECTM_MESSAGE_SIZE, &GettBuffer);
-
-	if(GettBuffer.adr == 7)
+static void checking()
+{
+	if(!NeedToSend)
 	{
+		receiveExactoDataStorage();
+
+		printf("iteration[%d]\n", ECTM_SendData_Counter);
+		if(exds_getData(ECTM_ReceiveBuffer, ECTM_MESSAGE_SIZE, 0) > 0)
+		{
+			memset(&GettBuffer, 0, sizeof(GettBuffer));
+			exlnk_getHeader(ECTM_ReceiveBuffer, ECTM_MESSAGE_SIZE, &GettBuffer);
+			if(GettBuffer.adr == 7)
+			{
+				NeedToSend = 1;
+			}
+		}
+	}
+}
+
+static void sending()
+{
+	if(NeedToSend)
+	{	
 		exlnk_cmd_str_t in;
 		exlnk_getCmd(&in, &GettBuffer.data[GettBuffer.datapt], GettBuffer.datalen);
+		NeedToSend = 0;
 
-    	printf(" Cnt: %d\nin:\nadr: %d\nval: \n", ECTM_SendData_Counter, in.address, in.value);
+		printf(" Cnt: %d\nin:\nadr: %d\nval: \n", ECTM_SendData_Counter, in.address, in.value);
 
 		exlnk_initHeader(&SendBuffer, ECTM_TransmitBuffer);
 		exlnk_fillHeader(&SendBuffer, 7, EXLNK_MSG_SIMPLE, EXLNK_PACK_SIMPLE, 0, ECTM_SendData_Counter, 0);
@@ -53,11 +76,15 @@ static void sending(int value)
 		exds_setData(SendBuffer.data, SendBuffer.pt_data, EX_THR_CTRL_OK);
 
 		transmitExactoDataStorage();
+
 	}
 }
 static void init()
 {
     ECTM_SendData_Counter = 0;
+ 	ex_subscribeOnEvent(&ExTimServicesInfo, ExTimServices, EX_THR_TIM, run_Tim_Lthread);
+
+	ex_setFreqHz(100);
 }
 int main(int argc, char *argv[]) 
 {
@@ -65,7 +92,7 @@ int main(int argc, char *argv[])
     init();
     while(1)
     {
-		sending(0);
+		sending();
 		sleep(2);
     }
     return 1;
