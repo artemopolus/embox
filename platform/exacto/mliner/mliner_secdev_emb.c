1a4fc3ec1106f532b1ae38c4ab87f584a8bb69cd
Artem <artem.o.kuznecov@gmail.com>
2022-10-27 18:15:08 +0300

    success build afina

---
+++ platform/exacto/mliner/mliner_secdev_emb.c
@@ -0,0 +1,187 @@
+#include "mliner/mliner_secdev.h"
+#include "mliner/mliner_secdev_impl.h"
+
+#include <errno.h>
+#include <embox/unit.h>
+#include <kernel/irq.h>
+#include <kernel/lthread/lthread.h>
+
+
+#include "gpio/gpio.h"
+
+uint8_t ReceiveBuffer[MLINE_RXTX_BUFFER_SIZE];
+uint8_t TransmitBuffer[MLINE_RXTX_BUFFER_SIZE];
+
+
+spi_mline_dev_t TransmitSpiDev;
+spi_mline_dev_t ReceiveSpiDev;
+
+static uint8_t SpiIsEnabled = 0;
+
+static irq_return_t TransmitIRQhandler(unsigned int irq_nr, void *data)
+{
+	if(runBoardSpiIRQhandlerTX())
+	{
+		TransmitSpiDev.isready = 1;
+	// 	    SPI2_FULL_DMA_tx_buffer.is_full = 0;
+   //  ExDtStr_Output_Storage[EX_THR_SPi_TX].isready = 1;
+	}
+   return IRQ_HANDLED;
+}
+STATIC_IRQ_ATTACH(15, TransmitIRQhandler, NULL);
+static irq_return_t ReceiveIRQhandler(unsigned int irq_nr, void *data)
+{
+	if(runBoardSpiIRQhandlerRX())
+	{
+		ReceiveSpiDev.isready = 1;
+	// 	    SPI2_FULL_DMA_rx_buffer.is_full = 1;
+   //  ExDtStr_Output_Storage[EX_THR_SPi_RX].isready = 1;
+   //  ExDtStr_Output_Storage[EX_THR_SPi_RX].result = EX_THR_CTRL_WAIT;
+   //  ex_updateCounter_ExDtStr(EX_THR_SPi_RX);
+	} 
+   return IRQ_HANDLED;
+}
+STATIC_IRQ_ATTACH(14, ReceiveIRQhandler, NULL);
+
+void enableSpiDevSec()
+{
+   irq_attach(15, TransmitIRQhandler, 0, NULL, "TransmitIRQhandler");
+   irq_attach(14, ReceiveIRQhandler, 0, NULL, "ReceiveIRQhandler");
+	enableBoardSpi(MLINE_RXTX_BUFFER_SIZE, MLINE_RXTX_BUFFER_SIZE);
+	SpiIsEnabled = 1;
+}
+void disableSpiDevSec()
+{
+	irq_detach(15,NULL);
+	irq_detach(14,NULL);
+	disableBoardSpi();
+	SpiIsEnabled = 0;
+}
+int downloadSpiDevSecData(uint32_t len)
+{
+	if(!ReceiveSpiDev.isready)
+		return 0;
+   pshsftPack_exbu8(ReceiveSpiDev.storage, ReceiveSpiDev.dmabufferdata, ReceiveSpiDev.dmabufferlen);
+	
+   for (int i = 0; i < ReceiveSpiDev.dmabufferlen; i++)
+   {
+      ReceiveSpiDev.dmabufferdata[i] = 0;
+   }
+   
+   
+   // for(uint32_t i = 0; i < ReceiveSpiDev.dmabufferlen; i++)
+	// {
+	// 	if(!pshsft_exbu8(ReceiveSpiDev.storage, ReceiveSpiDev.dmabufferdata[i]))
+	// 	{
+	// 		break;
+	// 	}
+	// }
+	return 1;
+}
+int uploadSpiDevSecData(uint32_t len)
+{
+	for(uint32_t i = 0; i < TransmitSpiDev.dmabufferlen; i++)
+	{
+		if(!grbfst_exbu8(TransmitSpiDev.storage, &TransmitSpiDev.dmabufferdata[i]))
+			break;
+	}
+	return 0;
+}
+EMBOX_UNIT_INIT(initSpiDevSec);
+static int initSpiDevSec(void)
+{
+	initBoardSpi();
+	TransmitSpiDev.dmabufferdata = TransmitBuffer;
+	TransmitSpiDev.dmabufferlen = MLINE_RXTX_BUFFER_SIZE;
+	TransmitSpiDev.processData = uploadSpiDevSecData;
+	// TransmitSpiDev.storage = &TransmitStore;
+	TransmitSpiDev.isfull = 0;
+	TransmitSpiDev.isready = 1;
+
+	ReceiveSpiDev.dmabufferdata = ReceiveBuffer;
+	ReceiveSpiDev.dmabufferlen = MLINE_RXTX_BUFFER_SIZE;
+	ReceiveSpiDev.processData = downloadSpiDevSecData;
+	// ReceiveSpiDev.storage = &ReceiveStore;
+	ReceiveSpiDev.isfull = 0;
+	ReceiveSpiDev.isready = 1;
+
+	setBoardSpiBuffer(&TransmitSpiDev, &ReceiveSpiDev);
+
+	SpiIsEnabled = 0;
+	return 0;
+}
+void setTxBuffSpiDevSec(ExactoBufferUint8Type * buffer)
+{
+	TransmitSpiDev.storage = buffer;
+}
+void setRxBuffSpiDevSec(ExactoBufferUint8Type * buffer)
+{
+	ReceiveSpiDev.storage = buffer;
+}
+void receiveSpiDevSec()
+{
+	if(SpiIsEnabled == 0)
+	{
+		initSpiDevSec();
+		enableSpiDevSec();
+	}
+		if(!ex_checkGpio(EX_GPIO_SPI_MLINE))
+	receiveBoardSpi(&ReceiveSpiDev);
+}
+uint8_t repeatTransmitSpiDevSec()
+{
+	if(!ex_checkGpio(EX_GPIO_SPI_MLINE))
+	{
+		resetBoardSpiRxTx(&ReceiveSpiDev, &TransmitSpiDev);
+		return 1;
+	}
+	return 0;
+}
+uint8_t transmitSpiDevSec()
+{
+	if(!ReceiveSpiDev.isready)
+	{
+		resetBoardSpiRx(&ReceiveSpiDev);	
+		return 0;
+	}
+	if(!ex_checkGpio(EX_GPIO_SPI_MLINE))
+	{
+		if(TransmitSpiDev.isready )
+		{
+			receiveTransmitBoardSpi(&ReceiveSpiDev, &TransmitSpiDev);
+			TransmitSpiDev.isready = 0;
+			ReceiveSpiDev.isready = 0;	
+			return 1;
+		}
+		else
+		{
+			receiveBoardSpi(&ReceiveSpiDev);
+		}
+	}
+	return 0;
+}
+uint16_t getReceivedDataSpiDevSec(uint8_t * trg, uint16_t trglen)
+{
+	if(downloadSpiDevSecData(ReceiveSpiDev.dmabufferlen))
+		return grball_exbu8(ReceiveSpiDev.storage, trg);
+	return 0;
+}
+uint16_t setTransmitDataSpiDevSec(uint8_t * src, uint16_t srclen)
+{
+	int i;
+	for(i = 0; i < srclen; i++)
+	{
+		if(!pshsft_exbu8(TransmitSpiDev.storage, src[i]))
+			break;
+	}
+	return i;
+}
+uint8_t getTransmitResultSpiDevSec()
+{
+	return TransmitSpiDev.isready;
+}
+uint8_t getReceiveResultSpiDevSec()
+{
+	return ReceiveSpiDev.isready;
+}
+
---
+++ platform/exacto/spi/mliner_maindev_impl_afina.c
@@ -0,0 +1,186 @@
+#include "mliner/mliner_maindev_impl.h"
+
+#include "gpio_config.h"
+
+int initBoardMlDev(void)
+{
+    LL_SPI_InitTypeDef SPI_InitStruct = {0};
+
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /* Peripheral clock enable */
+    //LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SPI1);
+    SPI_MLINE_ENABLE_CLOCK_SPI
+
+    //LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+    //LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+    SPI_MLINE_ENABLE_GPIO
+    /**SPI1 GPIO Configuration
+     PA5   ------> SPI1_SCK
+    PA7   ------> SPI1_MOSI
+    PB4   ------> SPI1_MISO
+    */
+    GPIO_InitStruct.Pin = SPI_MLINE_SCK_PIN;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.OutputType =    LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_5;
+    LL_GPIO_Init(SPI_MLINE_SCK_PORT, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = SPI_MLINE_MOSI_PIN;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.OutputType =    LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_5;
+    LL_GPIO_Init(SPI_MLINE_MOSI_PORT, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = SPI_MLINE_MISO_PIN;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.OutputType =    LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_5;
+    LL_GPIO_Init(SPI_MLINE_MISO_PORT, &GPIO_InitStruct);
+
+    /* SPI1 DMA Init */
+    /* SPI1_RX Init */
+    LL_DMA_SetChannelSelection      (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, SPI_MLINE_DMA_CHANNEL);
+    LL_DMA_SetDataTransferDirection (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
+    LL_DMA_SetStreamPriorityLevel   (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, LL_DMA_PRIORITY_LOW);
+    LL_DMA_SetMode          (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, LL_DMA_MODE_NORMAL);
+    LL_DMA_SetPeriphIncMode (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, LL_DMA_PERIPH_NOINCREMENT);
+    LL_DMA_SetMemoryIncMode (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, LL_DMA_MEMORY_INCREMENT);
+    LL_DMA_SetPeriphSize    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, LL_DMA_PDATAALIGN_BYTE);
+    LL_DMA_SetMemorySize    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, LL_DMA_MDATAALIGN_BYTE);
+    LL_DMA_DisableFifoMode  (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX);
+
+      /* SPI1_TX Init */
+    LL_DMA_SetChannelSelection      (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, SPI_MLINE_DMA_CHANNEL);
+    LL_DMA_SetDataTransferDirection (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, LL_DMA_DIRECTION_MEMORY_TO_PERIPH);
+    LL_DMA_SetStreamPriorityLevel   (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, LL_DMA_PRIORITY_LOW);
+    LL_DMA_SetMode          (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, LL_DMA_MODE_NORMAL);
+    LL_DMA_SetPeriphIncMode (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, LL_DMA_PERIPH_NOINCREMENT);
+    LL_DMA_SetMemoryIncMode (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, LL_DMA_MEMORY_INCREMENT);
+    LL_DMA_SetPeriphSize    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, LL_DMA_PDATAALIGN_BYTE);
+    LL_DMA_SetMemorySize    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, LL_DMA_MDATAALIGN_BYTE);
+    LL_DMA_DisableFifoMode  (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX);
+
+    SPI_InitStruct.TransferDirection = LL_SPI_FULL_DUPLEX;
+    SPI_InitStruct.Mode = LL_SPI_MODE_MASTER;
+    SPI_InitStruct.DataWidth = LL_SPI_DATAWIDTH_8BIT;
+    SPI_InitStruct.ClockPolarity = LL_SPI_POLARITY_HIGH;
+    SPI_InitStruct.ClockPhase = LL_SPI_PHASE_2EDGE;
+    SPI_InitStruct.NSS = LL_SPI_NSS_SOFT;
+    SPI_InitStruct.BaudRate = LL_SPI_BAUDRATEPRESCALER_DIV32;
+    SPI_InitStruct.BitOrder = LL_SPI_MSB_FIRST;
+    SPI_InitStruct.CRCCalculation = LL_SPI_CRCCALCULATION_DISABLE;
+    SPI_InitStruct.CRCPoly = 10;
+    LL_SPI_Init(SPI_MLINE_SPI, &SPI_InitStruct);
+
+    return 0;
+}
+
+void setBoardMlDevBuffer(
+	spi_mline_dev_t * transmit, spi_mline_dev_t * receive
+	// uint8_t * rxdata, uint32_t rxlen, uint8_t * txdata, uint32_t txlen, int (*download)(uint32_t len), int(*upload)(uint32_t len)
+	)
+{
+    LL_DMA_ConfigAddresses(SPI_MLINE_DMA, 
+                            SPI_MLINE_DMA_STREAM_RX,
+                           LL_SPI_DMA_GetRegAddr(SPI_MLINE_SPI),
+							(uint32_t)receive->dmabufferdata,
+                           LL_DMA_GetDataTransferDirection(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX));
+    LL_DMA_SetDataLength(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, receive->dmabufferlen );
+
+    LL_DMA_ConfigAddresses(SPI_MLINE_DMA,
+                           SPI_MLINE_DMA_STREAM_TX, 
+							(uint32_t)transmit->dmabufferdata,
+                           LL_SPI_DMA_GetRegAddr(SPI_MLINE_SPI),
+                           LL_DMA_GetDataTransferDirection(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX));
+    LL_DMA_SetDataLength(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, transmit->dmabufferlen);
+
+}
+
+uint8_t runBoardMlDevIRQhandlerRX(void)
+{
+    if (LL_DMA_IsActiveFlag_TC0(SPI_MLINE_DMA) != RESET)
+    {
+        LL_DMA_ClearFlag_TC0(SPI_MLINE_DMA);
+        return 1;
+    }
+    return 0;
+}
+uint8_t runBoardMlDevIRQhandlerTX(void)
+{
+    if (LL_DMA_IsActiveFlag_TC5(SPI_MLINE_DMA) != RESET)
+    {
+        LL_DMA_ClearFlag_TC5(SPI_MLINE_DMA);
+        return 1;
+    }
+    return 0;
+}
+void enableBoardMlDev(uint32_t rxlen, uint32_t txlen)
+{
+    LL_DMA_EnableIT_TC(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX);
+    LL_DMA_EnableIT_TE(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX);
+    LL_DMA_EnableIT_TC(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX);
+    LL_DMA_EnableIT_TE(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX);
+
+    LL_DMA_SetDataLength    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, txlen); //устанавливаем сколько символов передачть
+    LL_DMA_SetDataLength    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, rxlen); //устанавливаем сколько символов передачть
+    LL_SPI_EnableDMAReq_RX(SPI_MLINE_SPI);
+    LL_SPI_EnableDMAReq_TX(SPI_MLINE_SPI);
+    LL_DMA_EnableStream     (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX); //enable receive
+    LL_DMA_EnableStream     (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX); //enable transmit 
+    LL_SPI_Enable(SPI_MLINE_SPI);
+}
+void disableBoardMlDev(void)
+{
+    LL_DMA_DisableStream(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX); //enable receive
+    LL_DMA_DisableStream(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX); //enable transmit 
+    LL_SPI_Disable(SPI_MLINE_SPI);
+
+}
+void receiveBoardMlDev(spi_mline_dev_t * receiver)
+{
+    resetBoardMlDevRx(receiver);
+}
+void resetBoardMlDevRx(spi_mline_dev_t * receiver)
+{
+    LL_DMA_DisableStream(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX); //enable receive
+    LL_DMA_SetDataLength    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, receiver->dmabufferlen); //устанавливаем сколько символов передачть
+    LL_DMA_EnableStream     (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX); //enable receive
+}
+void resetBoardMlDevRxTx(spi_mline_dev_t * receiver, spi_mline_dev_t * transmit)
+{
+    LL_DMA_DisableStream(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX); //enable receive
+    LL_DMA_DisableStream(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX); //enable transmit 
+
+
+    LL_DMA_SetDataLength(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, transmit->dmabufferlen);
+    LL_DMA_SetDataLength    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, receiver->dmabufferlen); //устанавливаем сколько символов передачть
+
+    LL_DMA_EnableStream     (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX); //enable receive
+    LL_DMA_EnableStream     (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX); //enable transmit 
+
+}
+
+void receiveTransmitBoardMlDev(spi_mline_dev_t * receiver, spi_mline_dev_t * transmit)
+{
+    LL_DMA_DisableStream(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX); //enable receive
+    LL_DMA_DisableStream(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX); //enable transmit 
+
+    // if(receiver->collect_on)
+    //     receiver->collect(receiver->dmabufferdata, receiver->dmabufferlen);
+    receiver->processData(receiver->dmabufferlen);
+	transmit->processData(transmit->dmabufferlen);
+
+    LL_DMA_SetDataLength(SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX, transmit->dmabufferlen);
+    LL_DMA_SetDataLength    (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX, receiver->dmabufferlen); //устанавливаем сколько символов передачть
+
+    LL_DMA_EnableStream     (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_RX); //enable receive
+    LL_DMA_EnableStream     (SPI_MLINE_DMA, SPI_MLINE_DMA_STREAM_TX); //enable transmit 
+
+}
