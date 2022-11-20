58f589f0e87c6d3d36fffbdd4f711f135d38bc7e
Artem <artem.o.kuznecov@gmail.com>
2022-11-20 15:18:12 +0300

    tsts

--- platform/exacto/cmds/tests/bullet/MemTest.cpp
+++ platform/exacto/cmds/tests/bullet/MemTest.cpp
@@ -29,6 +29,7 @@
 int main(int argc, char** argv)
 {
 	int i;
+	#ifdef PART1
 	printf("Start0\n");
 	btDefaultCollisionConstructionInfo constructionInfo;
 
@@ -52,13 +53,60 @@ int main(int argc, char** argv)
 	printf("Start22 %d %d\n", tp, sz);
 	//here problem
 		m_persistentManifoldPool = new (mem) btPoolAllocator(sizeof(btPersistentManifold), constructionInfo.m_defaultMaxPersistentManifoldPoolSize);
+		#endif
 
-	printf("Start3\n");
+	btDefaultCollisionConstructionInfo constructionInfo;
+	constructionInfo.m_defaultMaxPersistentManifoldPoolSize = 16;
+	constructionInfo.m_defaultMaxCollisionAlgorithmPoolSize = 16;
 
-	// btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration(constructionInfo);
-	// printf("Start4\n");
+	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration(constructionInfo);
+	printf("Start4\n");
 
-	// ///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+	///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+
+	int m_dispatcherFlags;
+
+	btAlignedObjectArray<btPersistentManifold*> m_manifoldsPtr;
+
+	btNearCallback m_nearCallback;
+
+	btPoolAllocator* m_collisionAlgorithmPoolAllocator;
+
+	btPoolAllocator* m_persistentManifoldPoolAllocator;
+
+	btCollisionAlgorithmCreateFunc* m_doubleDispatchContactPoints[MAX_BROADPHASE_COLLISION_TYPES][MAX_BROADPHASE_COLLISION_TYPES];
+
+	btCollisionAlgorithmCreateFunc* m_doubleDispatchClosestPoints[MAX_BROADPHASE_COLLISION_TYPES][MAX_BROADPHASE_COLLISION_TYPES];
+
+	btCollisionConfiguration* m_collisionConfiguration;
+
+	
+	
+	int i;
+
+	// setNearCallback(defaultNearCallback);
+
+	m_collisionAlgorithmPoolAllocator = collisionConfiguration->getCollisionAlgorithmPool();
+	printf("Start5\n");
+
+	m_persistentManifoldPoolAllocator = collisionConfiguration->getPersistentManifoldPool();
+	printf("Start6\n");
+
+	for (i = 0; i < MAX_BROADPHASE_COLLISION_TYPES; i++)
+	{
+		printf("Start7:[%d]\n",i);
+		for (int j = 0; j < MAX_BROADPHASE_COLLISION_TYPES; j++)
+		{
+			printf("Start71:[%d]\n",j);
+			m_doubleDispatchContactPoints[i][j] = m_collisionConfiguration->getCollisionAlgorithmCreateFunc(i, j);
+			btAssert(m_doubleDispatchContactPoints[i][j]);
+			m_doubleDispatchClosestPoints[i][j] = m_collisionConfiguration->getClosestPointsAlgorithmCreateFunc(i, j);
+		}
+	}
+
+	printf("Start8\n");
+
+
 	// btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
 
 	printf("End\n");
